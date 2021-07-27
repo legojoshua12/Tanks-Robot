5386c754f8578aac89cb841fd600b62fc25ed9ce
legojoshua12 <legojoshua12@gmail.com>
2021-07-26 23:09:58 -0700

    Added board generation for a list and it populated it with random spots for players

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import os
-
 import discord
+
 from dotenv import load_dotenv
+
+import boardConstructor as bC
+import configUtils
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    numberOfPlayers = 5
+    boardData = bC.constructBoardData(numberOfPlayers)
+    boardData = bC.populateBoard(boardData, numberOfPlayers)
+    for row in range(len(boardData)):
+        print(boardData[row])
 
+
+# For first time boot of the robot,
+# it is a good idea to run even if the values exist to ensure that nothing gets messed up
+configUtils.initialize()
 client.run(TOKEN)
