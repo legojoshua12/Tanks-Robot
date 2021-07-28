028023ca24107791aacd7ca7ed936e3ad8acf94c
legojoshua12 <legojoshua12@gmail.com>
2021-07-27 21:08:54 -0700

    Moved commands to its own file

---
+++ commands.py
@@ -0,0 +1,111 @@
+import random
+
+import discord
+
+import configUtils
+
+
+async def direct_message_commands(message, command):
+    embedColor = int('0x' + ("%06x" % random.randint(0, 0xFFFFFF)), 0)
+    if command.startswith(configUtils.readValue('botSettings', 'botCommandPrefix')):
+        command = command[2:].lower()
+    else:
+        command = command.lower()
+    if command == 'help':
+        embedVar = discord.Embed(title="Command Reference", description="Here is a list of bot commands for your "
+                                                                        "reference! Simply type one of these to get "
+                                                                        "started.",
+                                 color=embedColor)
+        embedVar.add_field(name="help", value="Gives a list of commands", inline=False)
+        embedVar.add_field(name="rules", value="Gives the game rules and how to play", inline=False)
+        await message.channel.send(embed=embedVar)
+    elif command == 'rules':
+        emoji = '\U0001F52B'
+        embedVar = discord.Embed(title="Rules", description=f"This is the rules on how to play tanks! {emoji}",
+                                 color=embedColor)
+        embedVar.add_field(name="1. You use actions to do things", value="Actions are the the core of the game, "
+                                                                         "they can be used to move, shoot, or "
+                                                                         "increase your tank's range.",
+                           inline=False)
+        embedVar.add_field(name="2. You receive 1 action a day", value="Every player in the game receives 1 "
+                                                                       "action a day at 12pm (noon) PST",
+                           inline=False)
+        embedVar.add_field(name="3. Dead players can give an extra action",
+                           value="All dead players have the option to vote once a day. If a living player "
+                                 "receives 3+ votes, they will get 2 actions instead of 1 at 12pm.",
+                           inline=False)
+        embedVar.add_field(name="4. You have health",
+                           value="Each player has a total of 3 health points. If you lose a health point, "
+                                 "it cannot be regenerated and the damage is permanent.",
+                           inline=False)
+        embedVar.add_field(name="5. Actions can be given",
+                           value="You can bribe other players to give you actions. You are not bound by the "
+                                 "actions you receive each day and can get more from other players by asking them.",
+                           inline=False)
+        embedVar.add_field(name="6. You use actions whenever you want",
+                           value="Using your actions is not time bound. You may chose to use them at any point in "
+                                 "the day should you still be alive and have the available actions.",
+                           inline=False)
+        embedVar.add_field(name="7. Last person standing wins!",
+                           value="If you have the ability to make it to being the last person alive, you will "
+                                 "win! Congratulations if you manage to make it here!",
+                           inline=False)
+        await message.channel.send(embed=embedVar)
+    elif command == 'dm':
+        await message.author.send("I'm already here talking to you! Use `help` to get a list of commands.")
+    else:
+        await message.channel.send(message.author.mention + ' Unknown command. Please use `help` to view a '
+                                                            'list of commands and options.')
+
+async def public_commands(message, command):
+    embedColor = int('0x' + ("%06x" % random.randint(0, 0xFFFFFF)), 0)
+    if command == 'help':
+        commandPrefix = configUtils.readValue('botSettings', 'botCommandPrefix')
+        embedVar = discord.Embed(title="Command Reference", description="Here is a list of bot commands for your "
+                                                                        "reference! All commands may be done in "
+                                                                        "private as well using a direct message.",
+                                 color=embedColor)
+        embedVar.add_field(name=f"{commandPrefix}help", value="Gives a list of commands", inline=False)
+        embedVar.add_field(name=f"{commandPrefix}rules", value="Gives the game rules and how to play", inline=False)
+        embedVar.add_field(name=f"{commandPrefix}dm", value="Sends a direct message for privacy", inline=False)
+        await message.channel.send(embed=embedVar)
+    elif command == 'rules':
+        waveEmoji = '\U0001F52B'
+        embedVar = discord.Embed(title="Rules", description=f"This is the rules on how to play tanks! {waveEmoji}",
+                                 color=embedColor)
+        embedVar.add_field(name="1. You use actions to do things", value="Actions are the the core of the game, "
+                                                                         "they can be used to move, shoot, or "
+                                                                         "increase your tank's range.",
+                           inline=False)
+        embedVar.add_field(name="2. You receive 1 action a day", value="Every player in the game receives 1 "
+                                                                       "action a day at 12pm (noon) PST",
+                           inline=False)
+        embedVar.add_field(name="3. Dead players can give an extra action",
+                           value="All dead players have the option to vote once a day. If a living player "
+                                 "receives 3+ votes, they will get 2 actions instead of 1 at 12pm.",
+                           inline=False)
+        embedVar.add_field(name="4. You have health",
+                           value="Each player has a total of 3 health points. If you lose a health point, "
+                                 "it cannot be regenerated and the damage is permanent.",
+                           inline=False)
+        embedVar.add_field(name="5. Actions can be given",
+                           value="You can bribe other players to give you actions. You are not bound by the "
+                                 "actions you receive each day and can get more from other players by asking them.",
+                           inline=False)
+        embedVar.add_field(name="6. You use actions whenever you want",
+                           value="Using your actions is not time bound. You may chose to use them at any point in "
+                                 "the day should you still be alive and have the available actions.",
+                           inline=False)
+        embedVar.add_field(name="7. Last person standing wins!",
+                           value="If you have the ability to make it to being the last person alive, you will "
+                                 "win! Congratulations if you manage to make it here!",
+                           inline=False)
+        await message.channel.send(embed=embedVar)
+    elif command == 'dm':
+        letterEmoji = '\U00002709'
+        waveEmoji = '\U0001F44B'
+        await message.channel.send(message.author.mention + f' I just sent you a private message! {letterEmoji}')
+        await message.author.send(f"Hey there! {waveEmoji} How can I help you? Use `help` to get started!")
+    else:
+        await message.channel.send(message.author.mention + ' Unknown command. Please use `*/help` to view a '
+                                                            'list of commands and options.')
