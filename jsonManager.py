fbbf41fb734568e58f4d6cf23a1c92e78e03f91d
legojoshua12 <legojoshua12@gmail.com>
2021-07-28 21:00:13 -0700

    Updated to use a write to json for storing lobby data

--- jsonManager.py
+++ jsonManager.py
@@ -1,10 +1,50 @@
 import json
 
 
-def createGame(data):
-    print(data)
+def createGame(message):
+    players = {}
+    data = {'games': {message.guild.id: {message.channel.id: {}}}}
+    data['games'][message.guild.id][message.channel.id]['players'] = players
+    data['games'][message.guild.id][message.channel.id]['board'] = []
+    data['games'][message.guild.id][message.channel.id]['gameStatus'] = "lobby"
+    with open('Games.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    return data
+
+def addPlayerToGame(message, playerNumber):
+    data = __readJson()
+    newPlayerData = {
+        message.author.id: {
+            'playerNumber': playerNumber,
+            'lives': 3,
+            'actions': 0,
+            'range': 1
+        }
+    }
+    playersList = data['games'][str(message.guild.id)][str(message.channel.id)]['players']
+    playersList.update(newPlayerData)
+    data['games'][str(message.guild.id)][str(message.channel.id)]['players'] = playersList
     with open('Games.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def addPlayerToGame(player):
-    print(f'Adding player {player}')
+def getNumberOfPlayersInGame(message):
+    data = __readJson()
+    numberOfPlayers = 0
+    playersList = data['games'][str(message.guild.id)][str(message.channel.id)]['players']
+    for player in playersList:
+        numberOfPlayers = numberOfPlayers + 1
+    return numberOfPlayers
+
+def checkIfGameIsInChannel(message):
+    data = __readJson()
+    try:
+        gameState = data['games'][str(message.guild.id)][str(message.channel.id)]['gameStatus']
+        return gameState
+    except:
+        return 'none'
+
+def __readJson():
+    file = open('Games.json', )
+    data = json.load(file)
+    file.close()
+    return data
