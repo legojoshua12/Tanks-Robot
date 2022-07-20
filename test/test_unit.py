9c3ae3552ec12dd2d70be02f65b00ba93c1712db
legojoshua12 <legojoshua12@gmail.com>
2022-07-19 23:37:23 -0700

    Fixed unit test for board populator to actual test but the test is failing

--- test/test_unit.py
+++ test/test_unit.py
@@ -12,4 +12,13 @@ def test_boardPopulator():
     players = [0, 1, 2, 3, 4]
     board = bc.constructBoardData(len(players))
     board = bc.populateBoard(board, 5)
-    assert len(board) == 10
+    foundPlayers = []
+    for row in board:
+        for col in board[row]:
+            if board[row][col] is not 0:
+                for fp in foundPlayers:
+                    if fp == board[row][col]:
+                        break
+                foundPlayers.append(board[row][col])
+                break
+    assert len(foundPlayers) == 5
