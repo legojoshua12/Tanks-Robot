7105511db52ff21a4e739e681c6ed1eb66781311
legojoshua12 <legojoshua12@gmail.com>
2023-02-01 19:28:58 +0000

    Removed time from integration tests for lint

--- test/test_integration.py
+++ test/test_integration.py
@@ -1,8 +1,6 @@
 """
 All tests related to discord and checking to make sure that the robot is interacting with the discord servers correctly
 """
-import time
-
 import discord
 import discord.ext.test as dpytest
 import pytest
