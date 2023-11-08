a21e1e75706b7224978390a64f787ea943dbb871
legojoshua12 <legojoshua12@gmail.com>
2023-11-08 13:30:59 -0800

    Added test for many commands being sent and verify there is no connection pool leaks

--- test/integration/test_active_game.py
+++ test/integration/test_active_game.py
@@ -41,6 +41,23 @@ async def test_help(bot, command_prefix, mock_cursor):
 
 
 @pytest.mark.asyncio
+async def test_many_repeat_commands(bot, command_prefix, mock_cursor):
+    await utils.JsonUtility.start_sample_game(bot, mock_cursor)
+    channel = bot.guilds[0].text_channels[0]
+    for _ in range(1000):
+        await channel.send(f"{command_prefix}help")
+        mess = dpytest.get_message()
+        mess.author = bot.guilds[0].members[2]
+
+        await messageHandler.handle_message(mess, bot, command_prefix)
+
+        embeds = dpytest.get_message(peek=True).embeds
+        assert len(embeds) == 1
+        new_embed = commands.active_game_help_embed(embeds[0].color, command_prefix)
+        assert dpytest.verify().message().contains().embed(new_embed)
+
+
+@pytest.mark.asyncio
 async def test_rules_not_in_game(bot, command_prefix, mock_cursor):
     await utils.JsonUtility.start_sample_game(bot, mock_cursor)
     channel = bot.guilds[0].text_channels[0]
